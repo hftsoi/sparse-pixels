c9d59633cc2069fbf9754e402419398de2e895a6
Ho Fung Tsoi <hftsoi0411@gmail.com>
2025-06-24 21:59:40 -0400

    test model

---
+++ hls_proj/sparsemnist/hls_full2/firmware/weights/b6.h
@@ -0,0 +1,16 @@
+//Numpy array shape [1]
+//Min -0.125000000000
+//Max -0.125000000000
+//Number of zeros 0
+
+#ifndef B6_H_
+#define B6_H_
+
+#ifndef __SYNTHESIS__
+bias6_t b6[1];
+#else
+bias6_t b6[1] = {-0.12500};
+
+#endif
+
+#endif
