6096f86d7699fd98539388da16473a9c669ee877
Ho Fung Tsoi <hftsoi0411@gmail.com>
2025-06-30 16:38:54 -0400

    templates for scaling study

---
+++ hls_proj/scaling/relu-p10-template/firmware/weights/w2.h
@@ -0,0 +1,16 @@
+//Numpy array shape [3, 3, 1, 2]
+//Min -1.000000000000
+//Max 0.968750000000
+//Number of zeros 0
+
+#ifndef W2_H_
+#define W2_H_
+
+#ifndef __SYNTHESIS__
+weight2_t w2[9];
+#else
+weight2_t w2[9] = {-0.96875, -0.15625, -0.44000, 0.15625, 0.95875, 0.18750, -0.13200, -0.62500, 0.84375};
+
+#endif
+
+#endif
