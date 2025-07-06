d68a9deed2738e85ed912c667e94d191799a2e3e
Ho Fung Tsoi <hftsoi0411@gmail.com>
2025-07-05 23:18:45 -0400

    hls for sparse flatten scaling

---
+++ hls_proj/scaling/flatten/flatten-c1-out200/flatten-p5-c1-out200/firmware/weights/w2.h
@@ -0,0 +1,16 @@
+//Numpy array shape [3, 3, 1, 2]
+//Min -1.000000000000
+//Max 0.968750000000
+//Number of zeros 0
+
+#ifndef W2_H_
+#define W2_H_
+
+#ifndef __SYNTHESIS__
+weight2_t w2[9];
+#else
+weight2_t w2[9] = {0.99609375, -0.5390625, 0.67578125, -0.890625, -0.02734375, 0.78515625, -0.1640625, -1.01171875, 1.43359375};
+
+#endif
+
+#endif
