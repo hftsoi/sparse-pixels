7ab2099625e6763137efae0c4a1679d6b3190488
Ho Fung Tsoi <hftsoi0411@gmail.com>
2025-07-12 00:31:25 -0400

    trial hls proj

---
+++ hls_proj/sparsemnist/model/hls_sparse_h/firmware/weights/b14.h
@@ -0,0 +1,16 @@
+//Numpy array shape [10]
+//Min -0.406250000000
+//Max 0.375000000000
+//Number of zeros 1
+
+#ifndef B14_H_
+#define B14_H_
+
+#ifndef __SYNTHESIS__
+bias14_t b14[10];
+#else
+bias14_t b14[10] = {-0.21875, 0.03125, 0.28125, -0.09375, 0.21875, 0.37500, -0.09375, 0.00000, -0.40625, -0.18750};
+
+#endif
+
+#endif
---
+++ hls_proj/sparsemnist/model/hls_sparse_l/firmware/weights/b14.h
@@ -0,0 +1,16 @@
+//Numpy array shape [10]
+//Min -0.468750000000
+//Max 0.718750000000
+//Number of zeros 1
+
+#ifndef B14_H_
+#define B14_H_
+
+#ifndef __SYNTHESIS__
+bias14_t b14[10];
+#else
+bias14_t b14[10] = {-0.09375, 0.71875, 0.03125, -0.18750, 0.00000, 0.25000, -0.28125, 0.25000, -0.46875, -0.12500};
+
+#endif
+
+#endif
