938b74627ffbd033b7522d691d7ef47a0fe19044
Ho Fung Tsoi <hftsoi0411@gmail.com>
2025-07-11 23:19:53 -0400

    hls proj for sparsemnist

---
+++ hls_proj/sparsemnist/model/hls_full2/firmware/weights/b6.h
@@ -0,0 +1,16 @@
+//Numpy array shape [2]
+//Min 0.031250000000
+//Max 0.437500000000
+//Number of zeros 0
+
+#ifndef B6_H_
+#define B6_H_
+
+#ifndef __SYNTHESIS__
+bias6_t b6[2];
+#else
+bias6_t b6[2] = {0.43750, 0.03125};
+
+#endif
+
+#endif
---
+++ hls_proj/sparsemnist/model/hls_sparse_m/firmware/weights/b14.h
@@ -0,0 +1,16 @@
+//Numpy array shape [10]
+//Min -0.406250000000
+//Max 0.406250000000
+//Number of zeros 0
+
+#ifndef B14_H_
+#define B14_H_
+
+#ifndef __SYNTHESIS__
+bias14_t b14[10];
+#else
+bias14_t b14[10] = {-0.28125, 0.40625, 0.06250, -0.18750, 0.06250, 0.21875, 0.06250, 0.25000, -0.40625, -0.03125};
+
+#endif
+
+#endif
