c7e0f6d02d68ea964270d907bfb862102d8e7de1
Ho Fung Tsoi <hftsoi0411@gmail.com>
2025-07-17 16:31:26 -0400

    test hls

---
+++ test4/firmware/weights/b6.h
@@ -0,0 +1,16 @@
+//Numpy array shape [3]
+//Min -0.046875000000
+//Max 0.195312500000
+//Number of zeros 0
+
+#ifndef B6_H_
+#define B6_H_
+
+#ifndef __SYNTHESIS__
+bias6_t b6[3];
+#else
+bias6_t b6[3] = {-0.0468750, 0.1953125, 0.1640625};
+
+#endif
+
+#endif
