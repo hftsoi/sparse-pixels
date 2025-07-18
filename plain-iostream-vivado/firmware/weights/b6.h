c51bac53717d410940cc3fa9baf3accf6fc1023d
Ho Fung Tsoi <hftsoi0411@gmail.com>
2025-07-17 22:08:37 -0400

    test hls

---
+++ plain-iostream-vivado/firmware/weights/b6.h
@@ -0,0 +1,16 @@
+//Numpy array shape [3]
+//Min -0.250000000000
+//Max 0.187500000000
+//Number of zeros 0
+
+#ifndef B6_H_
+#define B6_H_
+
+#ifndef __SYNTHESIS__
+bias6_t b6[3];
+#else
+bias6_t b6[3] = {-0.2500000, -0.0781250, 0.1875000};
+
+#endif
+
+#endif
