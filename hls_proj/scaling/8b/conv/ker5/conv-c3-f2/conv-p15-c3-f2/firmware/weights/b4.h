a61ed773af6f6660bf8c112f7bb8233de7a6f41c
Ho Fung Tsoi <hftsoi0411@gmail.com>
2025-07-22 13:09:22 -0400

    hls proj for scaling

---
+++ hls_proj/scaling/8b/conv/ker5/conv-c3-f2/conv-p15-c3-f2/firmware/weights/b4.h
@@ -0,0 +1,16 @@
+//Numpy array shape [2]
+//Min 0.312500000000
+//Max 0.750000000000
+//Number of zeros 0
+
+#ifndef B4_H_
+#define B4_H_
+
+#ifndef __SYNTHESIS__
+bias4_t b4[2];
+#else
+bias4_t b4[2] = {-0.671875, -1.75};
+
+#endif
+
+#endif
