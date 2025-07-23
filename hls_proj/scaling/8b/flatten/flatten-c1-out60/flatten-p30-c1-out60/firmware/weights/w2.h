e97af49af6e0401adc10a851c65fb0fe83119459
Ho Fung Tsoi <hftsoi0411@gmail.com>
2025-07-23 13:34:50 -0400

    hls proj for scaling

---
+++ hls_proj/scaling/8b/flatten/flatten-c1-out60/flatten-p30-c1-out60/firmware/weights/w2.h
@@ -0,0 +1,16 @@
+//Numpy array shape [3, 3, 1, 2]
+//Min -1.000000000000
+//Max 0.968750000000
+//Number of zeros 0
+
+#ifndef W2_H_
+#define W2_H_
+
+#ifndef __SYNTHESIS__
+weight2_t w2[9];
+#else
+weight2_t w2[9] = {1.96875, -0.734375, 0.046875, -1.25, 0.859375, -0.015625, 0.5, 1.125, -1.953125};
+
+#endif
+
+#endif
