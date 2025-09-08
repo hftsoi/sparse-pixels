850eab34160931bb465e17b93c150db7f61ab758
Ho Fung Tsoi <hftsoi0411@gmail.com>
2025-09-08 13:21:49 -0400

    hls proj for scaling 16b conv ker5

---
+++ hls_proj/scaling/16b/conv/ker5/conv-c1-f3/conv-p25-c1-f3/firmware/weights/b2.h
@@ -0,0 +1,16 @@
+//Numpy array shape [2]
+//Min 0.312500000000
+//Max 0.750000000000
+//Number of zeros 0
+
+#ifndef B2_H_
+#define B2_H_
+
+#ifndef __SYNTHESIS__
+bias2_t b2[3];
+#else
+bias2_t b2[3] = {0.669921875, -9.525390625, 5.1171875};
+
+#endif
+
+#endif
---
+++ hls_proj/scaling/16b/conv/ker5/conv-c2-f3/conv-p20-c2-f3/firmware/weights/b4.h
@@ -0,0 +1,16 @@
+//Numpy array shape [2]
+//Min 0.312500000000
+//Max 0.750000000000
+//Number of zeros 0
+
+#ifndef B4_H_
+#define B4_H_
+
+#ifndef __SYNTHESIS__
+bias4_t b4[3];
+#else
+bias4_t b4[3] = {-3.57421875, 11.083984375, -8.73046875};
+
+#endif
+
+#endif
