bca94c7ef246b499a41fffb3c5e661537a2f6784
Ho Fung Tsoi <hftsoi0411@gmail.com>
2025-09-08 13:51:19 -0400

    hls proj for scaling 16b flatten

---
+++ hls_proj/scaling/16b/flatten/flatten-c1-out80/flatten-p20-c1-out80/firmware/weights/w2.h
@@ -0,0 +1,16 @@
+//Numpy array shape [3, 3, 1, 2]
+//Min -1.000000000000
+//Max 0.968750000000
+//Number of zeros 0
+
+#ifndef W2_H_
+#define W2_H_
+
+#ifndef __SYNTHESIS__
+weight2_t w2[9];
+#else
+weight2_t w2[9] = {3.845703125, -7.126953125, 1.572265625, -12.44921875, 0.927734375, -2.763671875, 5.3828125, -0.48828125, 14.251953125};
+
+#endif
+
+#endif
