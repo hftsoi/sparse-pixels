366c557ef7bfbbefc6f10c9e66b13db532210318
Ho Fung Tsoi <hftsoi0411@gmail.com>
2026-01-18 11:04:28 -0500

    rm env.sh

--- env.sh
+++
@@ -1,7 +0,0 @@
-source /tools/Xilinx/Vitis_HLS/2023.1/settings64.sh
-sudo ln -s /usr/lib/x86_64-linux-gnu/crti.o /usr/lib64/
-sudo ln -s /usr/lib/x86_64-linux-gnu/crt1.o /usr/lib64/
-sudo ln -s /usr/lib/x86_64-linux-gnu/libpthread* /usr/lib64/
-sudo ln -s /usr/lib/x86_64-linux-gnu/libm* /usr/lib64/
-sudo ln -s /usr/lib/x86_64-linux-gnu/libc* /usr/lib64/
-sudo ln -s /usr/lib/x86_64-linux-gnu/crtn.o /usr/lib64/
