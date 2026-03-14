1fa9bbdd417c2957185c06022a669e7b7666eeb2
Ho Fung Tsoi <hftsoi0411@gmail.com>
2026-03-13 22:53:25 -0400

    qkeras->hgq2

--- sparsepixels/layers.py
+++ sparsepixels/layers.py
@@ -1,93 +1,97 @@
-import tensorflow as tf
-from qkeras import QConv2D, quantizers
-from tensorflow.keras.layers import AveragePooling2D, MaxPooling2D
+import keras
+from keras import ops
+from keras.layers import AveragePooling2D, MaxPooling2D
+from hgq.layers import QConv2D
+from hgq.quantizer import Quantizer
+from hgq.quantizer.config import QuantizerConfig
 
-class InputReduce(tf.keras.layers.Layer):
+
+class InputReduce(keras.layers.Layer):
     def __init__(self, n_max_pixels, threshold, **kwargs):
-        super(InputReduce, self).__init__(**kwargs)
+        super().__init__(**kwargs)
         self.n_max_pixels = n_max_pixels
         self.threshold = threshold
 
     def call(self, inputs):
-        batch_size = tf.shape(inputs)[0]
-        h = tf.shape(inputs)[1]
-        w = tf.shape(inputs)[2]
-
-        '''
-        if self.threshold is not None:
-            cond = inputs > self.threshold
-        else:
-            cond = inputs != 0
-        active_flag = tf.cast(tf.reduce_any(cond, axis=-1), tf.int32)
-        '''
+        batch_size = ops.shape(inputs)[0]
+        h = ops.shape(inputs)[1]
+        w = ops.shape(inputs)[2]
 
         # to be consistent with hls, check only the first input channel
         if self.threshold is not None:
-            active_flag = tf.cast(inputs[..., 0] > self.threshold, tf.int32)
+            active_flag = ops.cast(inputs[..., 0] > self.threshold, "int32")
         else:
-            active_flag = tf.cast(inputs[..., 0] != 0, tf.int32)
+            active_flag = ops.cast(inputs[..., 0] != 0, "int32")
 
-        active_flag_flat = tf.reshape(active_flag, [batch_size, h * w])
-        active_count = tf.cumsum(active_flag_flat, axis=1)
+        active_flag_flat = ops.reshape(active_flag, [batch_size, h * w])
+        active_count = ops.cumsum(active_flag_flat, axis=1)
 
-        keep_mask_flat = tf.cast(tf.logical_and(active_flag_flat == 1, active_count <= self.n_max_pixels), inputs.dtype)
-        keep_mask = tf.reshape(keep_mask_flat, [batch_size, h, w, 1])
+        keep_mask_flat = ops.cast(
+            ops.logical_and(active_flag_flat == 1, active_count <= self.n_max_pixels),
+            inputs.dtype,
+        )
+        keep_mask = ops.reshape(keep_mask_flat, [batch_size, h, w, 1])
 
         inputs_reduced = inputs * keep_mask
         return inputs_reduced, keep_mask
 
     def get_config(self):
-        config = super(InputReduce, self).get_config()
+        config = super().get_config()
         config.update({
             "n_max_pixels": self.n_max_pixels,
-            "threshold": self.threshold
+            "threshold": self.threshold,
         })
         return config
-    
 
-class RemoveDilatedPixels(tf.keras.layers.Layer):
+
+class RemoveDilatedPixels(keras.layers.Layer):
     def __init__(self, **kwargs):
-        super(RemoveDilatedPixels, self).__init__(**kwargs)
+        super().__init__(**kwargs)
 
     def call(self, inputs):
         x, mask = inputs
-        mask = tf.cast(mask, x.dtype)
-        removed = x * mask
-        return removed
+        mask = ops.cast(mask, x.dtype)
+        return x * mask
 
     def get_config(self):
-        config = super(RemoveDilatedPixels, self).get_config()
-        return config
+        return super().get_config()
 
 
-class QConv2DSparse(tf.keras.layers.Layer):
+class QConv2DSparse(keras.layers.Layer):
     def __init__(self, *conv_args, **conv_kwargs):
         super().__init__(name=conv_kwargs.get("name", None))
-        self._bias_quant_cfg = conv_kwargs.pop("bias_quantizer", None)
-        self._bias_quantizer = (quantizers.get_quantizer(self._bias_quant_cfg) if self._bias_quant_cfg is not None else None)
+        self._bq_conf = conv_kwargs.pop("bq_conf", None)
 
         conv_kwargs["use_bias"] = False
+        conv_kwargs.setdefault("enable_iq", False)
         self.conv = QConv2D(*conv_args, **conv_kwargs)
-        self.bias = self.add_weight(
-            name = "bias",
-            shape = (self.conv.filters,),
-            initializer = "zeros",
-            trainable = True,
-            dtype = self.conv.dtype,
-        )
         self.masker = RemoveDilatedPixels()
+
+    def build(self, input_shape):
+        self.sparse_bias = self.add_weight(
+            name="sparse_bias",
+            shape=(self.conv.filters,),
+            initializer="zeros",
+            trainable=True,
+        )
+        if self._bq_conf is not None:
+            self._bq = Quantizer(self._bq_conf, name=f"{self.name}_bq")
+            self._bq.build((self.conv.filters,))
+        else:
+            self._bq = None
+        super().build(input_shape)
 
     def call(self, inputs, **kwargs):
         x, keep_mask = inputs
         x = self.masker((x, keep_mask))
         y = self.conv(x, **kwargs)
 
-        b = self.bias
-        if self._bias_quantizer is not None:
-            b = self._bias_quantizer(b)
-        b = tf.reshape(b, shape=(1, 1, 1, -1))
+        b = self.sparse_bias
+        if self._bq is not None:
+            b = self._bq(b)
+        b = ops.reshape(b, (1, 1, 1, -1))
 
-        non_zero = tf.cast(tf.not_equal(y, 0), y.dtype)
+        non_zero = ops.cast(y != 0, y.dtype)
         y = y + b * non_zero
 
         y = self.masker((y, keep_mask))
@@ -96,18 +100,17 @@ class QConv2DSparse(tf.keras.layers.Layer):
     def get_config(self):
         cfg = super().get_config()
         cfg["conv_config"] = self.conv.get_config()
-        cfg["bias_quantizer"] = self._bias_quant_cfg
+        cfg["bq_conf"] = self._bq_conf
         return cfg
 
     @classmethod
     def from_config(cls, config):
         conv_cfg = config.pop("conv_config")
-        bias_quant_cfg = config.pop("bias_quantizer", None)
-        layer = cls(**conv_cfg, bias_quantizer=bias_quant_cfg)
-        return layer
+        bq_conf = config.pop("bq_conf", None)
+        return cls(**conv_cfg, bq_conf=bq_conf)
 
 
-class AveragePooling2DSparse(tf.keras.layers.Layer):
+class AveragePooling2DSparse(keras.layers.Layer):
     def __init__(self, *pool_args, **pool_kwargs):
         super().__init__(name=pool_kwargs.get("name", None))
         self.avg_pool = AveragePooling2D(*pool_args, **pool_kwargs)
@@ -128,9 +131,9 @@ class AveragePooling2DSparse(tf.keras.layers.Layer):
     def from_config(cls, config):
         pool_cfg = config.pop("pool_config")
         return cls(**pool_cfg)
-    
 
-class MaxPooling2DSparse(tf.keras.layers.Layer):
+
+class MaxPooling2DSparse(keras.layers.Layer):
     def __init__(self, *pool_args, **pool_kwargs):
         super().__init__(name=pool_kwargs.get("name", None))
         self.max_pool = MaxPooling2D(*pool_args, **pool_kwargs)
@@ -150,4 +153,3 @@ class MaxPooling2DSparse(tf.keras.layers.Layer):
     def from_config(cls, config):
         pool_cfg = config.pop("pool_config")
         return cls(**pool_cfg)
-
--- tests/test_model.py
+++ tests/test_model.py
@@ -1,56 +1,58 @@
-from sparsepixels.layers import *
-from qkeras import *
-from tensorflow.keras.layers import *
+import keras
+from keras.layers import Flatten, Activation, AveragePooling2D, ReLU
+from hgq.layers import QConv2D, QDense
+from hgq.config import QuantizerConfigScope, LayerConfigScope
+from hgq.quantizer.config import QuantizerConfig
+from sparsepixels.layers import InputReduce, QConv2DSparse, AveragePooling2DSparse
 
-def build_cnn(is_sparse, B=16, I=6, n_max_pixels=None):
-    quantizer = quantized_bits(B, I, alpha=1)
-    quantized_relu = f'quantized_relu({B}, {I})'
 
-    x_in = keras.Input(shape=(32, 32, 1), name='x_in')
-    if is_sparse:
-        x, keep_mask = InputReduce(n_max_pixels=n_max_pixels, threshold=1, name='input_reduce')(x_in)
-    else:
-        x = x_in
+def build_cnn(is_sparse, n_max_pixels=None):
+    with (
+        QuantizerConfigScope(place='all', default_q_type='kbi', overflow_mode='SAT_SYM'),
+        QuantizerConfigScope(place='datalane', default_q_type='kif', overflow_mode='WRAP'),
+        LayerConfigScope(enable_ebops=False, enable_iq=False),
+    ):
+        x_in = keras.Input(shape=(32, 32, 1), name='x_in')
+        if is_sparse:
+            x, keep_mask = InputReduce(n_max_pixels=n_max_pixels, threshold=1, name='input_reduce')(x_in)
+        else:
+            x = x_in
 
-    if is_sparse:
-        x = QConv2DSparse(filters=1, kernel_size=7, use_bias=True, name='conv1', padding='same', strides=1,
-                          kernel_quantizer=quantizer, bias_quantizer=quantizer)([x, keep_mask])
-        x = QActivation(quantized_relu, name='relu1')(x)
-        x, keep_mask = AveragePooling2DSparse(4, name='pool1')([x, keep_mask])
+        if is_sparse:
+            x = QConv2DSparse(filters=1, kernel_size=7, name='conv1', padding='same', strides=1,
+                              bq_conf=QuantizerConfig('default', 'bias'))([x, keep_mask])
+            x = ReLU(name='relu1')(x)
+            x, keep_mask = AveragePooling2DSparse(4, name='pool1')([x, keep_mask])
 
-        x = QConv2DSparse(filters=3, kernel_size=5, use_bias=True, name='conv2', padding='same', strides=1,
-                          kernel_quantizer=quantizer, bias_quantizer=quantizer)([x, keep_mask])
-        x = QActivation(quantized_relu, name='relu2')(x)
-        x, keep_mask = AveragePooling2DSparse(2, name='pool2')([x, keep_mask])
+            x = QConv2DSparse(filters=3, kernel_size=5, name='conv2', padding='same', strides=1,
+                              bq_conf=QuantizerConfig('default', 'bias'))([x, keep_mask])
+            x = ReLU(name='relu2')(x)
+            x, keep_mask = AveragePooling2DSparse(2, name='pool2')([x, keep_mask])
+        else:
+            x = QConv2D(filters=1, kernel_size=7, name='conv1', padding='same', strides=1,
+                        activation='relu')(x)
+            x = AveragePooling2D(4, name='pool1')(x)
 
-    else:
-        x = QConv2D(filters=1, kernel_size=7, use_bias=True, name='conv1', padding='same', strides=1,
-                    kernel_quantizer=quantizer, bias_quantizer=quantizer)(x)
-        x = QActivation(quantized_relu, name='relu1')(x)
-        x = AveragePooling2D(4, name='pool1')(x)
-        
-        x = QConv2D(filters=3, kernel_size=5, use_bias=True, name='conv2', padding='same', strides=1,
-                    kernel_quantizer=quantizer, bias_quantizer=quantizer)(x)
-        x = QActivation(quantized_relu, name='relu2')(x)
-        x = AveragePooling2D(2, name='pool2')(x)
+            x = QConv2D(filters=3, kernel_size=5, name='conv2', padding='same', strides=1,
+                        activation='relu')(x)
+            x = AveragePooling2D(2, name='pool2')(x)
 
-    x = Flatten(name='flatten')(x)
+        x = Flatten(name='flatten')(x)
 
-    x = QDense(36, kernel_quantizer=quantizer, bias_quantizer=quantizer, name='dense1')(x)
-    x = QActivation(quantized_relu, name='relu3')(x)
+        x = QDense(36, name='dense1', activation='relu')(x)
 
-    x = QDense(10, kernel_quantizer=quantizer, bias_quantizer=quantizer, name='dense2')(x)
-    x = Activation('softmax', name='softmax')(x)
+        x = QDense(10, name='dense2')(x)
+        x = Activation('softmax', name='softmax')(x)
 
     return keras.Model(x_in, x)
 
 
 def test_build_full_cnn():
     cnn_full = build_cnn(is_sparse=False)
-    cnn_full.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001), loss='categorical_crossentropy', metrics = ['accuracy'])
+    cnn_full.compile(optimizer=keras.optimizers.Adam(learning_rate=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
     cnn_full.summary()
 
 def test_build_sparse_cnn():
     cnn_sparse = build_cnn(is_sparse=True, n_max_pixels=20)
-    cnn_sparse.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001), loss='categorical_crossentropy', metrics = ['accuracy'])
+    cnn_sparse.compile(optimizer=keras.optimizers.Adam(learning_rate=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
     cnn_sparse.summary()
